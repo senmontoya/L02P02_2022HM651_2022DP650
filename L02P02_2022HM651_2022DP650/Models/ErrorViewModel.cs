924683473fab5e9add6dbd49fec8fa8e7277f256
DamianDev-Heaven <harryadonay62@gmail.com>
2025-04-01 17:32:18 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2022HM651_2022DP650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2022HM651_2022DP650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ L02P02_2022HM651_2022DP650/Models/LibreriaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022HM651_2022DP650.Models
+{
+    public class LibreriaContext : DbContext
+    {
+        public LibreriaContext(DbContextOptions<LibreriaContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
